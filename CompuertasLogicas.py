1c5ca690ec4c8a38dfb98717b2afe8eeeabe75cf
CDCM <cristianzdmc14@gmail.com>
2023-12-08 21:48:58 -0500

    Update CompuertasLogicas.py

--- CompuertasLogicas.py
+++ CompuertasLogicas.py
@@ -1,18 +1,25 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow  # Ventana principal
-from PyQt5.QtCore import QUrl # Para abrir una URL
-from PyQt5.QtWebEngineWidgets import QWebEngineView # Para mostrar una pagina web
-import sys 
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtGui import QIcon
 #pip install PyQt5 PyQtWebEngine
-class MainWindow(QMainWindow):
-    def __init__(self): 
-        super(MainWindow, self).__init__() # Constructor de la clase padre
-        self.browser = QWebEngineView() # Crea un objeto de tipo QWebEngineView
-        self.browser.setUrl(QUrl('http://163.10.22.82/OAS/compuertas_logicas/Simulacion/editor_simple.html'))
-        self.setCentralWidget(self.browser) # Agrega el objeto browser al centro de la ventana
-        self.setGeometry(100, 100, 800, 450) # (x, y, ancho, alto)
-        self.show() # Muestra la ventana
 
-def main():
-    app = QApplication(sys.argv) # Crea un objeto de tipo QApplication
-    window = MainWindow() # Crea un objeto de tipo MainWindow
-    app.exec_() # Ejecuta la aplicación    
+URL = 'http://163.10.22.82/OAS/compuertas_logicas/Simulacion/editor_simple.html' 
+WINDOW_SIZE = (100, 100, 800, 450)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl(URL))
+        self.setWindowIcon(QIcon('img/logic.ico'))
+        self.setWindowTitle("Simulador de compuertas lógicas")
+        self.setCentralWidget(self.browser)
+        self.setGeometry(*WINDOW_SIZE)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec_()
